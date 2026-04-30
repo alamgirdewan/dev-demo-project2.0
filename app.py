914561b37bdef81dc7154d37c88b9a2b5a9f405d
alamgirdewan <alamgirdeeewan@gmail.com>
2026-04-30 11:46:28 +0600

    deleting comment

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+import requests
+import random
+import math
+import config
 
+app = Flask(__name__)
+CORS(app)
